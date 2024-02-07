c304e7de4d13d7be6fdab6aea08291af74a4ff9f
melissa <melissameb29@gmail.com>
2024-02-07 14:39:10 +0100

    connect to the databse

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+//congig dotenv
+dotenv.config();
+
+//database
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("databse connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//server
 const app = express();
-
 app.listen(3000, () => {
   console.log("server is running");
 });

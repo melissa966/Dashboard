0391ebc3992033b28abaf17ebfc4b8f5c0afbfb3
melissa <melissameb29@gmail.com>
2024-02-07 16:02:17 +0100

    create middlware and function to handle errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,16 +13,16 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fileds are required"));
   }
 
-  const hashPassword =  bcryptjs.hashSync(password,10)
+  const hashPassword = bcryptjs.hashSync(password, 10);
   try {
-    const newUser = new User({ username, email, password: hashPassword});
+    const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
     res.json("Success");
   } catch (error) {
     console.error("Error in signup:", error);
-    res.status(500).json({ message: "An error occurred while signing up" });
+    next(error);
   }
 };

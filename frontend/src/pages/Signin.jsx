3769cbb66bed1fe691b2a01323099ad9674eb73b
melissa <melissameb29@gmail.com>
2024-02-08 11:29:33 +0100

    complete sign in fonctionality handle submit

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,4 +1,3 @@
-import LockIcon from "@mui/icons-material/Lock";
 import {
   Avatar,
   Grid,
@@ -9,29 +8,48 @@ import {
   Button,
   Typography,
 } from "@mui/material";
-import { useForm } from "react-hook-form";
 
+import CircularProgress from "@mui/material/CircularProgress"; 
+
+import LockIcon from "@mui/icons-material/Lock";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export default function Signin() {
+  const [loading, setLoading] = useState(false);
   const {
     register,
-    handleSubmit,
-    watch,
+    handleSubmit: handleFormSubmit, 
     formState: { errors },
   } = useForm({
     defaultValues: {
-      email:"",
+      email: "",
       password: "",
     },
   });
-
-  console.log(watch());
-
-  const onSubmit = (data) => {
-    console.log(data);
+  const navigate = useNavigate();
+  const onSubmit = async (formData) => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setLoading(false);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleFormSubmit(onSubmit)}>
       <Grid
         container
         justifyContent="center"
@@ -39,7 +57,10 @@ export default function Signin() {
         style={{ minHeight: "100vh" }}
       >
         <Grid item>
-          <Paper elevation={10} style={{ padding: 30, width: 350, margin: "auto" }}>
+          <Paper
+            elevation={10}
+            style={{ padding: 30, width: 350, margin: "auto" }}
+          >
             <Grid container direction="column" alignItems="center" spacing={2}>
               <Grid item>
                 <Avatar style={{ backgroundColor: "#3739d7" }}>
@@ -47,18 +68,18 @@ export default function Signin() {
                 </Avatar>
               </Grid>
               <Grid item>
-                <Typography variant="h5">Signin</Typography>
+                <Typography variant="h5">Sign in</Typography>
               </Grid>
 
               <Grid item style={{ width: "100%" }}>
                 <TextField
                   {...register("email", { required: "This is required" })}
                   id="email"
-                  label="email*"
+                  label="Email*"
                   variant="standard"
                   placeholder="Enter your email"
                   fullWidth
-                  error={errors.email?.message}
+                  error={!!errors.email}
                   helperText={errors.email?.message}
                 />
               </Grid>
@@ -71,7 +92,7 @@ export default function Signin() {
                   placeholder="Enter your password"
                   type="password"
                   fullWidth
-                  error={errors.password?.message}
+                  error={!!errors.password}
                   helperText={errors.password?.message}
                 />
               </Grid>
@@ -79,12 +100,16 @@ export default function Signin() {
                 <FormControlLabel control={<Checkbox />} label="Remember me" />
               </Grid>
               <Grid item style={{ width: "100%" }}>
-                <Button variant="contained" type="submit" fullWidth>
-                  Login
+                <Button
+                  variant="contained"
+                  type="submit"
+                  fullWidth
+                  disabled={loading}
+                >
+                  {loading ? <CircularProgress /> : "login"}
                 </Button>
               </Grid>
-              <Grid item style={{ width: "100%", textAlign: "left" }}>
-              </Grid>
+              <Grid item style={{ width: "100%", textAlign: "left" }}></Grid>
             </Grid>
           </Paper>
         </Grid>

107dc2c47ab88735ca589951e8a2288352495f6b
melissa <melissameb29@gmail.com>
2024-02-08 10:36:52 +0100

    create signin api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
-import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
+import User from "../models/UserModel.js";
+
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -23,6 +25,41 @@ export const signup = async (req, res, next) => {
     res.json("Success");
   } catch (error) {
     console.error("Error in signup:", error);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "Tous les champs sont obligatoires"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      next(errorHandler(404, "Utilisateur non trouvé"));
+    }
+
+    const validPassword = bcryptjs.compare(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Mot de passe invalide"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,107 @@
+import LockIcon from "@mui/icons-material/Lock";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Typography,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 
+export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      user: "",
+      email:"",
+      password: "",
+    },
+  });
 
-export default function signup() {
+  console.log(watch());
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>signup</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item>
+          <Paper elevation={10} style={{ padding: 30, width: 350, margin: "auto" }}>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+              <Grid item>
+                <Avatar style={{ backgroundColor: "#3739d7" }}>
+                  <LockIcon />
+                </Avatar>
+              </Grid>
+              <Grid item>
+                <Typography variant="h5">Signup</Typography>
+              </Grid>
+              <Grid item style={{ width: "100%" }}>
+                <TextField
+                  {...register("user", { required: "This is required" })}
+                  id="username"
+                  label="Username*"
+                  variant="standard"
+                  placeholder="Enter your username"
+                  fullWidth
+                  error={errors.user?.message}
+                  helperText={errors.user?.message}
+                />
+              </Grid>
+
+              <Grid item style={{ width: "100%" }}>
+                <TextField
+                  {...register("email", { required: "This is required" })}
+                  id="email"
+                  label="email*"
+                  variant="standard"
+                  placeholder="Enter your email"
+                  fullWidth
+                  error={errors.email?.message}
+                  helperText={errors.email?.message}
+                />
+              </Grid>
+              <Grid item style={{ width: "100%" }}>
+                <TextField
+                  {...register("password", { required: "This is required" })}
+                  id="password"
+                  label="Password*"
+                  variant="standard"
+                  placeholder="Enter your password"
+                  type="password"
+                  fullWidth
+                  error={errors.password?.message}
+                  helperText={errors.password?.message}
+                />
+              </Grid>
+              <Grid item style={{ width: "100%", textAlign: "left" }}>
+                <FormControlLabel control={<Checkbox />} label="Remember me" />
+              </Grid>
+              <Grid item style={{ width: "100%" }}>
+                <Button variant="contained" type="submit" fullWidth>
+                  Register
+                </Button>
+              </Grid>
+              <Grid item style={{ width: "100%", textAlign: "left" }}>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </form>
+  );
 }
